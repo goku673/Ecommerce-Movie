0da8a8cd8b208c3c09945dd171eea74547c076e9
Freddy Amin Zapata <83481744+goku673@users.noreply.github.com>
2025-01-08 13:41:04 -0400

    changed to alter true

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const server = require('./src/app');
 const PORT = process.env.PORT || 3002;
 
 // Cambia force: true a simplemente conn.sync() o conn.sync({ alter: true })
-conn.sync() // O conn.sync({ alter: true }) si necesitas ajustar la estructura
+conn.sync({alter : true}) // O conn.sync({ alter: true }) si necesitas ajustar la estructura
     .then(() => {
         server.listen(PORT, () => {
             console.log(`Escuchando en el puerto ${PORT}`);
