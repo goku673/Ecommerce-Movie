562b64e6f5f51a6609a4fabd151d808d77f83b9b
Freddy Amin Zapata <83481744+goku673@users.noreply.github.com>
2025-01-07 21:57:18 -0400

    fixed  config.vite.js

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -77,6 +77,7 @@ const Home = () => {
     localStorage.removeItem("user");
     dispatch(setUser (null));
     setIsModalOpen(false);
+    navigate("/");
   };
 
   const NavItemsData = [
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // Esto asegura que las rutas relativas funcionen
+  build: {
+    outDir: 'dist' // Carpeta de salida para el build
+  },
+  server: {
+    // Esto es opcional, pero ayuda en el desarrollo local
+    host: true
+  }
+});
