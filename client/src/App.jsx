eb5fc7d87a8ee77fadd06c71d92f48be57aa0a84
Freddy Amin Zapata <83481744+goku673@users.noreply.github.com>
2025-01-10 19:56:03 -0400

    component Pagination deleted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import LandingPage from './Components/LandingPage';
-import Contact from './Components/Contact';
 import Detail from './Components/Detail/Detail';
 import FavoritesMovies from './Components/FavoritesMovies';
 import Resenias from './Components/Resenias';
@@ -16,7 +15,7 @@ function App() {
           <Route path='favorites' element={<FavoritesMovies />} />
           <Route path='resenias' element={<Resenias />} />
         </Route>
-        <Route path='/contact' element={<Contact />} />
+       
         <Route path='/peliculaDetail/:id' element={<Detail />} />
       </Routes>
     </>
